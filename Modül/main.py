e4921afc04ec5fd2edc1df2bacfce3e032db203d
Alican Tarım <alicantarim@hotmail.com>
2024-03-10 18:05:39 +0300

    Kendi modülünü oluşturma

--- Modül/main.py
+++ Modül/main.py
@@ -1,1 +1,15 @@
 import mod
+
+# result = help(mod)
+# result = help(mod.func)
+
+result = mod.number
+result = mod.numbers
+result = mod.person['name']
+result = mod.person['age']
+result = mod.func(10)
+
+p = mod.Person()
+p.speak()   # "I am speaking." ekrana yazar.
+
+print(result) 
